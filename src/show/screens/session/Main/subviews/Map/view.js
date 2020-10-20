221581a463014327d4812cc66e490a033b3dcd43
Bogdan Pop <contact@webraptor.eu>
2020-10-20 20:07:32 +0300

    MM-259 When zooming in and out, the map "bounces"

--- src/show/screens/session/Main/subviews/Map/view.js
+++ src/show/screens/session/Main/subviews/Map/view.js
@@ -43,7 +43,7 @@ const regionChangeComplete = (
       });
     });
     if (isGesture) {
-      setUserIsInteracting(false);
+      setTimeout(setUserIsInteracting.bind(null, false), 500);
     }
   }
 };
