345546683a5d52b9b5ee7c04d250ee1c5423394f
Bogdan Pop <contact@webraptor.eu>
2020-12-03 14:16:31 +0200

    Update amplitude payloads according to latest API / governance

--- src/process/services/analytics/index.js
+++ src/process/services/analytics/index.js
@@ -10,9 +10,10 @@ const amplitude = new Amplitude(Config.AMPLITUDE_KEY);
 let identifiers = {};
 
 const getIdentifiers = () => {
-  const { device } = store().store.getState();
+  const { device, user } = store().store.getState();
   return {
-    device_id: device.uniqueID
+    device_id: device.uniqueID,
+    user_id: user.driverId
   };
 };
 
@@ -21,23 +22,27 @@ export const EVENTS = events;
 export default {
   trackEvent: (eventName, eventData) => {
     if (JSON.parse(Config.TRACK_ANALYTICS_EVENTS)) {
-      const { device_id = null } = identifiers;
-      if (device_id === null) {
+      const { device_id = null, user_id = null } = identifiers;
+      if (device_id === null || user_id === null) {
         identifiers = getIdentifiers();
       }
 
-      amplitude.track({
-        event_properties: {
-          ...eventData
-        },
+      amplitude.track(
+        {
+          event_properties: {
+            ...eventData
+          },
 
-        app_version: `${Config.APP_VERSION_NAME}-${Config.APP_VERSION_CODE}`,
-        device_id,
-        event_type: `${Config.AMPLITUDE_EVENT_NAME_PREFIX}::${eventName}`,
-        os_version: Platform.Version,
-        platform: Platform.OS,
-        timestamp: new Date()
-      });
+          app_version: `${Config.APP_VERSION_NAME}-${Config.APP_VERSION_CODE}`,
+          device_id,
+          event_type: `${Config.AMPLITUDE_EVENT_NAME_PREFIX}::${eventName}`,
+          os_version: Platform.Version,
+          platform: Platform.OS,
+          timestamp: new Date(),
+          user_id
+        },
+        { min_id_length: 1 }
+      );
     }
   }
 };
