5b0041835a054abcf93258e1d2dddfcd902aca18
Bogdan Pop <contact@webraptor.eu>
2021-02-02 18:21:02 +0200

    disable appcenter crashes on development

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import { enableES5 } from 'immer';
+import Crashes from 'appcenter-crashes';
+import Config from 'react-native-config';
 import { AppRegistry } from 'react-native';
 
 import Application from './src/show/Application';
@@ -13,7 +15,11 @@ if (global.location && global.location.pathname.includes('/debugger-ui')) {
   global.FormData = global.originalFormData || global.FormData;
 }
 
+if (Config.ENVIRONMENT === 'development') {
+  Crashes.setEnabled(false);
+}
+
 AppRegistry.registerComponent(appName, () => Application);
 
-//eslint-disable-next-line
+// eslint-disable-next-line no-console
 console.disableYellowBox = true;
