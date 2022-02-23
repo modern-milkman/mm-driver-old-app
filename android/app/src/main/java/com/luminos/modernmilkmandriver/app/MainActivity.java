557b527ec3798b43fd54c35bf4e9b01aba8bef9a
Emanuel Boros <emanuel.boros@luminos.software>
2022-02-23 09:04:07 +0000

    DAT-894 Android crash fix after camera permisions
    * Android crash fix after camera permisions
    
    
    Approved-by: Darryl Kay
    Approved-by: Bogdan Pop

--- android/app/src/main/java/com/luminos/modernmilkmandriver/app/MainActivity.java
+++ android/app/src/main/java/com/luminos/modernmilkmandriver/app/MainActivity.java
@@ -3,10 +3,16 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactRootView;
 import com.facebook.react.ReactActivityDelegate;
 import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+import android.os.Bundle;
 
 import expo.modules.ReactActivityDelegateWrapper;
 
 public class MainActivity extends ReactActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(null);
+  }
+  
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
