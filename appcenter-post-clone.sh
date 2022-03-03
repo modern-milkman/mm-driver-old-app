c2cd141fcbbfd5e046bc3993cdf6f30533c0d810
Bogdan Pop <contact@webraptor.eu>
2022-03-03 15:08:52 +0200

    append appcenter build IDs to test / uat builds

---
+++ appcenter-post-clone.sh
@@ -0,0 +1,9 @@
+CUR_SPACE=.
+
+if [ "$APPEND_APPCENTER_BUILD_ID" = "true" ]
+then
+  printf "Adding APPCENTER_BUILD_ID to version number"
+  printf "\n\n"
+  version=`grep -E "\"version\":\s?\".*\"" $CUR_SPACE/package.json | sed s/[^0-9.]//g`;
+  sed -i -e 's/"version":.*/"version": "'$version'.'$APPCENTER_BUILD_ID'",/' $CUR_SPACE/package.json
+fi
