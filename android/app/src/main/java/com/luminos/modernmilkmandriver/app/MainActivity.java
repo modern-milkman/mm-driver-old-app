e559db625bf4c29952b04356f53241677f2fdb4e
Bogdan Pop <bogdan.pop@luminos.software>
2023-07-05 09:27:07 +0000

    TMM-3123 HOTFIX androidx.fragment.app.Fragment.instantiate android crashes * TMM-3123 HOTFIX androidx.fragment.app.Fragment.instantiate android crashes
    Approved-by: Emanuel Boros

--- android/app/src/main/java/com/luminos/modernmilkmandriver/app/MainActivity.java
+++ android/app/src/main/java/com/luminos/modernmilkmandriver/app/MainActivity.java
@@ -6,8 +6,14 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
+import android.os.Bundle;
 
 public class MainActivity extends ReactActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(null);
+  }
+
   @Override
   public void onNewIntent(Intent intent) {
       super.onNewIntent(intent);
