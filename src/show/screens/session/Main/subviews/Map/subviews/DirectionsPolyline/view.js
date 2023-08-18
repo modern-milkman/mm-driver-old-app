8402ed7adf7989aacae0bbd39e7bdc75d4c050d8
Bogdan Pop <bogdan.pop@luminos.software>
2023-08-18 10:54:38 +0000

    TMM-3164 Fix iOS directions polyline colors
    * bump version
    Approved-by: Emanuel Boros

--- src/show/screens/session/Main/subviews/Map/subviews/DirectionsPolyline/view.js
+++ src/show/screens/session/Main/subviews/Map/subviews/DirectionsPolyline/view.js
@@ -1,8 +1,15 @@
+/*
+TODO
+the strokeColor code can be changed to colors.inputSecondary only once we upgrade to RN .72+ with Appearance.setColorScheme available
+Appearance.setColorScheme should be called on app init to force OS elements to display in the app's darkMode preference
+*/
+
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Platform } from 'react-native';
+import { Appearance, Platform } from 'react-native';
 import { Polyline } from 'react-native-maps';
 
+import { RGB_COLORS } from 'Theme';
 import { useTheme } from 'Containers';
 
 const DirectionsPolyline = ({ directionsPolyline }) => {
@@ -15,7 +22,7 @@ const DirectionsPolyline = ({ directionsPolyline }) => {
         strokeColor={
           Platform.OS === 'android'
             ? colors.inputSecondary
-            : colors.inputPrimary
+            : `rgb(${RGB_COLORS.inputSecondary[Appearance.getColorScheme()]})`
         }
         coordinates={directionsPolyline}
         geodesic
